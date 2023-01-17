61f2f47ab02d1d995d0e036ab9b6934bf5ae1f0e
Alissa Cohan <alissacohan18@gmail.com>
2023-01-16 19:40:07 -0500

    created Employee test and code

---
+++ __tests__/Employee.test.js
@@ -0,0 +1,13 @@
+const Employee = require("../lib/Employee");
+
+test("creates an Employee object", () => {
+    const employee = new Employee ("Shirley", "131546", "Shirley123@madeup.com");
+
+    expect(employee.name).toBe("Shirley");
+    expect(employee.id).toEqual("131546");
+    expect(employee.email).toEqual("Shirley123@madeup.com");
+    expect(employee.getName()).toBe("Shirley");
+    expect(employee.getId()).toBe("131546");
+    expect(employee.getEmail()).toBe("Shirley123@madeup.com");
+    expect(employee.getRole()).toBe("Employee");
+});
